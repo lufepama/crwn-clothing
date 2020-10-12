4c055aebe342ff1c4fa6bdafd94391222833acca
lufepama <lufepama31@gmail.com>
2020-10-12 19:51:34 +0200

    Updates

---
+++ precache-manifest.a7f451da980a409a1aeb8ea5a06c94ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19c017c5305ed8128ac49a9f50b6df4",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "dab14b8a3b666c99ef80",
+    "url": "/crwn-clothing/static/css/main.b6526d5a.chunk.css"
+  },
+  {
+    "revision": "5393c17d03dfad1c0eae",
+    "url": "/crwn-clothing/static/js/2.83a4caf2.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/crwn-clothing/static/js/2.83a4caf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab14b8a3b666c99ef80",
+    "url": "/crwn-clothing/static/js/main.55dce82c.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
